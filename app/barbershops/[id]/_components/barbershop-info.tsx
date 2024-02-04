df105f1be18ec1763283f6870c556e8b23e270d0
jonasvictor <jonas.victor@dcx.ufpb.br>
2024-02-04 02:32:24 -0300

    feat: ensure unauthenticated user is redirected to login booking click

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -2,6 +2,8 @@ import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
 import { Service } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
     params: {
@@ -10,6 +12,8 @@ interface BarbershopDetailsPageProps {
 }
 
 const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+    const session = await getServerSession(authOptions);
+
     if (!params.id) {
         // TODO: Redirecionar para a home page
         return <h1>Barbershop not found</h1>;
@@ -32,7 +36,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
         <BarbershopInfo barbershop={barbershop} />
 
         {barbershop.services.map((service: Service) => (
-            <ServiceItem key={service.id} service={service} />
+            <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user}/>
         ))}
     </div>
 }
